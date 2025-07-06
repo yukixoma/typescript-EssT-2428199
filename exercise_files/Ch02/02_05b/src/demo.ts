eb5f255f049d4858857cf7d8e52fde58ef1eece9
yukixoma <yukixoma08@gmail.com>
2025-07-06 17:14:34 +0900

    Typing functions

--- exercise_files/Ch02/02_05b/src/demo.ts
+++ exercise_files/Ch02/02_05b/src/demo.ts
@@ -1,8 +1,23 @@
 interface Contact {
     id: number;
     name: string;
+    clone(source: Contact): Contact
 }
 
-function clone(source) {
+function clone(source: Contact) : Contact {
     return Object.apply({}, source);
 }
+
+// Function as function argument
+function funcAsArgument(source: Contact, func: (arg: Contact) => Contact) : Contact {
+    return func(source);
+}
+
+const contact: Contact = {
+    id: 1,
+    name: "Phat",
+    clone: clone
+}
+
+const contact1 = clone(contact);
+const contact2 = funcAsArgument(contact, clone);
