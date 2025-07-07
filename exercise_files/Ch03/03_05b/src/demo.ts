220c9511c7f2e9148eea8eaa7323efb067d39a98
yukixoma <yukixoma08@gmail.com>
2025-07-07 18:24:04 +0900

    Defining dynamic but limited types with records

--- exercise_files/Ch03/03_05b/src/demo.ts
+++ exercise_files/Ch03/03_05b/src/demo.ts
@@ -1,4 +1,4 @@
-let x = { name: "Wruce Bayne" };
+let x : Record<string, string | number> = { name: "Wruce Bayne" };
 x.id = 1234;
 
 
@@ -25,9 +25,9 @@ interface Query {
     matches(val): boolean;
 }
 
-function searchContacts(contacts: Contact[], query) {
+function searchContacts(contacts: Contact[], query: Record<keyof Contact, Query>) {
     return contacts.filter(contact => {
-        for (const property of Object.keys(contact)) {
+        for (const property of Object.keys(contact) as Array<keyof Contact>) {
             // get the query object for this property
             const propertyQuery = query[property];
             // check to see if it matches
@@ -45,6 +45,7 @@ const filteredContacts = searchContacts(
     {
         id: { matches: (id) => id === 123 },
         name: { matches: (name) => name === "Carol Weaver" },
-        phoneNumber: { matches: (name) => name === "Carol Weaver" },
+        status: { matches: (status) => status === "active"},
+        address: { matches: (address) => address === "address"}
     }
 );
